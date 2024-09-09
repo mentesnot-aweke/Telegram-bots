a6a5299cd94a60239c987b62bf0592c8771c77ef
mentesnot-aweke <mentesnotaweke2545@gmial.com>
2024-09-09 17:13:26 +0300

    updated home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="bg-black w-screen h-screen p-2">
+    <div className="bg-black w-screen h-screen p-2 flex-col ">
       <div className="  bg-gray-900 mt-24 p-2 rounded-md flex justify-around">
         <img
           src="src\assets\images\shootingStar.png "
@@ -10,6 +10,11 @@ function App() {
           className="h-8 w-6 -ml-3"
         />
         <button className="bg-gray-700 px-2 rounded-3xl ml-2">open</button>
+      </div>
+      <div className="justify-items-stretch">
+        <button className="bg-white text-black mt-24 p-2 mr-1 rounded-md   w-full absolute bottom-12  ">
+          Start Farming
+        </button>
       </div>
     </div>
   );
